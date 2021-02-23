b8fa6029f1cc33e4e85bf9a5adbb0cc33b084638
Chiara Aengevelt <chiara@aengevelt.com>
2021-02-23 21:01:45 +0100

    Added Star rating

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,22 +1,29 @@
 class VotesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @votes = Vote.all
   end
   
   def new
     @property = Property.find(params[:property_id])
+    @project = Project.find(params[:project_id])
     @vote = Vote.new
+    @vote.user = current_user
   end
 
   def create
     @vote = Vote.new(vote_params)
     @property = Property.find(params[:property_id])
-    @user = current_user
+    @project = Project.find(params[:project_id])
+
+    # @user = current_user
     @vote.property = @property
-    @vote.user = @user
+    # @vote.property.project_id = @project.id
+    @vote.user = current_user
 
     if @vote.save!
-      redirect_to property_votes_path
+      redirect_to project_properties_path
     else
       flash[:alert] = "Something went wrong."
       render :new
