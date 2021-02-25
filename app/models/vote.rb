d05643f8ba928a7ee2218c5c1514a72a31990249
Chiara Aengevelt <chiara@aengevelt.com>
2021-02-25 03:47:08 +0100

    added stage functionality, enabled voted on different stages and display votes form others

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :property
 
-  validates :property_id, uniqueness: { scope: :user_id }
-
+  validates :property_id, uniqueness: { scope: [:user_id, :stage] }
 
 end
