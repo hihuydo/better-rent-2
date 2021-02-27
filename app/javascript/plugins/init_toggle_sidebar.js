d1b97f7071a2642d64cbebfb6a1e97a4b7e30cf2
Huy Do <hihuydo@gmail.com>
2021-02-27 18:05:24 +0100

    small changes

--- app/javascript/plugins/init_toggle_sidebar.js
+++ app/javascript/plugins/init_toggle_sidebar.js
@@ -1,29 +1,31 @@
 const initToogleSidebar = () => {
-  const home = document.getElementById("home");
+  const home = document.querySelector("#home li");
   const project = document.getElementById("project");
-  const property = document.getElementById("property");
+  const property = document.querySelector("#property li");
+
+  console.log(project);
+
+  // var url = window.location.pathname;
+  //   $('ul.navbar-nav a[href="' + url + '"]').parent().addClass('active');
+  // });
 
   if (home) {
   project.addEventListener( "click", () => {
-    $(project).addClass("active").siblings().removeClass("active");
-    // $(home).removeClass("active");
-    // property.removeClass("active");
-    console.log("test");
+    $(home).addClass("active").siblings().removeClass("active");
    });
   home.addEventListener("click", () => {
     $(home).addClass("active").siblings().removeClass("active");
+    console.log("hi");
   });
   property.addEventListener("click", () => {
     $(property).addClass("active").siblings().removeClass("active");
   });
-
 };
 
+// $(document).ready(function(){
+//    $(".active").removeClass("active");
+//    $("project").addClass("active");
+// });
+}
 
 export { initToogleSidebar };
-
-
-
-
-
-
