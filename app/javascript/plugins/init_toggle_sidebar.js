9752d76d71dbd764731bc2bfa5f51fd66d251d8d
Huy Do <hihuydo@gmail.com>
2021-02-27 12:13:25 +0100

    seed properties with images

--- app/javascript/plugins/init_toggle_sidebar.js
+++ app/javascript/plugins/init_toggle_sidebar.js
@@ -1,23 +1,29 @@
-
 const initToogleSidebar = () => {
   const home = document.getElementById("home");
   const project = document.getElementById("project");
   const property = document.getElementById("property");
+
   if (home) {
-    project.addEventListener( "click", () => {
-      $(project).addClass("active").siblings().removeClass("active");
-      // $(home).removeClass("active");
-      // property.removeClass("active");
-      console.log("test");
-     });
-    home.addEventListener("click", () => {
-      $(home).addClass("active").siblings().removeClass("active");
-    });
-    property.addEventListener("click", () => {
-      $(property).addClass("active").siblings().removeClass("active");
-    });
-  };
+  project.addEventListener( "click", () => {
+    $(project).addClass("active").siblings().removeClass("active");
+    // $(home).removeClass("active");
+    // property.removeClass("active");
+    console.log("test");
+   });
+  home.addEventListener("click", () => {
+    $(home).addClass("active").siblings().removeClass("active");
+  });
+  property.addEventListener("click", () => {
+    $(property).addClass("active").siblings().removeClass("active");
+  });
+
 };
 
 
 export { initToogleSidebar };
+
+
+
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require "open-uri"
 
 Participant.destroy_all
 Property.destroy_all
@@ -39,10 +39,11 @@ project_1 = Project.create!(
 )
 puts "Project: #{project_1.name} created"
 
-property_1 = Property.create!(
+property_1_photos = ['https://res.cloudinary.com/lewagon-training/image/upload/v1614183343/6t9isjgbclf48cpcdiw85sv0h9vz.png']
+property_1 = Property.new(
   project_id: project_1.id,
   user_id: user_1.id,
-  title: 'Office at friedrichstrasse',
+  title: 'Office at Friedrichstraße',
   street: 'Friedrichstraße',
   number: '55',
   zipcode: '10117 Berlin',
@@ -50,9 +51,89 @@ property_1 = Property.create!(
   lettable_area: 4500,
   rent_per_sqm: 25,
   active: true,
-  cloudinary_id: 'ws768u81ducfgn8hpqdv5z5uggiu'
 )
+property_1_photos.each do |img|
+  property_1.photos.attach(io: URI.open(img), filename: 'friedrichstrasse.png', content_type: 'image/png')
+end
+property_1.save
 puts "Property: #{property_1.street} created"
+
+property_2_photos = ['https://res.cloudinary.com/lewagon-training/image/upload/v1614182828/28uu70gvfuef1mkbd6u7uzla5ayq.jpg']
+property_2 = Property.new(
+  project_id: project_1.id,
+  user_id: user_1.id,
+  title: 'Office at Münzstraße',
+  street: 'Münzstraße',
+  number: '55',
+  zipcode: '10117 Berlin',
+  city: 'Berlin',
+  lettable_area: 4500,
+  rent_per_sqm: 25,
+  active: true,
+)
+property_2_photos.each do |img|
+  property_2.photos.attach(io: URI.open(img), filename: 'muenzstrasse.jpg', content_type: 'image/jpg')
+end
+property_2.save
+puts "Property: #{property_2.street} created"
+
+property_3_photos = ['https://res.cloudinary.com/lewagon-training/image/upload/v1614183491/bas1bztz1a28lvf1djnqdexcwd4j.png']
+property_3 = Property.new(
+  project_id: project_1.id,
+  user_id: user_1.id,
+  title: 'Office at Gormannstraße',
+  street: 'Gormannstraße',
+  number: '55',
+  zipcode: '10117 Berlin',
+  city: 'Berlin',
+  lettable_area: 4500,
+  rent_per_sqm: 25,
+  active: true,
+)
+property_3_photos.each do |img|
+  property_3.photos.attach(io: URI.open(img), filename: 'gormannstrasse.png', content_type: 'image/png')
+end
+property_3.save
+puts "Property: #{property_3.street} created"
+
+property_4_photos = ['https://res.cloudinary.com/lewagon-training/image/upload/v1614105130/vn767o8lact2j800k1g60l60qf39.jpg']
+property_4 = Property.new(
+  project_id: project_1.id,
+  user_id: user_1.id,
+  title: 'Office at Weinmeisterstraße',
+  street: 'Weinmeisterstraße',
+  number: '55',
+  zipcode: '10117 Berlin',
+  city: 'Berlin',
+  lettable_area: 4500,
+  rent_per_sqm: 25,
+  active: true,
+)
+property_4_photos.each do |img|
+  property_4.photos.attach(io: URI.open(img), filename: 'weinmeisterstrasse.jpg', content_type: 'image/jpg')
+end
+property_4.save
+puts "Property: #{property_4.street} created"
+
+property_5_photos = ['https://res.cloudinary.com/lewagon-training/image/upload/v1614277868/cjw23okksve64xmb2vdjk9a4k8sk.jpg']
+property_5 = Property.new(
+  project_id: project_1.id,
+  user_id: user_1.id,
+  title: 'Office at Torstraße',
+  street: 'Torstraße',
+  number: '55',
+  zipcode: '10117 Berlin',
+  city: 'Berlin',
+  lettable_area: 4500,
+  rent_per_sqm: 25,
+  active: true,
+)
+property_5_photos.each do |img|
+  property_5.photos.attach(io: URI.open(img), filename: 'torstrasse.jpg', content_type: 'image/jpg')
+end
+property_5.save
+puts "Property: #{property_5.street} created"
+
 
 
 participants_1 = Participant.create!(
