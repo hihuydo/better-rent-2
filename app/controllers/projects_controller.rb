8b672b56cd012e47439534d2216521e243c9232a
Chiara Aengevelt <chiara@aengevelt.com>
2021-02-27 09:51:42 +0100

    started with pundit in project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,27 +2,39 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @projects = Project.all
+    # function checked and needed
+    @projects = policy_scope(Project).order(created_at: :desc)
+    @user = current_user
   end
 
-  def show
-    @project = Project.find(params[:id])
-    @properties = @project.properties # probably needs to be refactored to get all properties of that project
-    @markers = @properties.geocoded.map do |prop|
-      {
-        lat: prop.latitude,
-        lng: prop.longitude
-      }
-    end
-  end
+  # Do we use this? 
+  
+  # def show
+  #   @project = Project.find(params[:id])
+  #   @properties = @project.properties # probably needs to be refactored to get all properties of that project
+  #   @markers = @properties.geocoded.map do |prop|
+  #     {
+  #       lat: prop.latitude,
+  #       lng: prop.longitude
+  #     }
+  #   end
+  # end
 
   def new
+    # function checked and needed
+    # everyone can create a project
     @project = Project.new
+    @user = current_user
+    authorize @project
   end
 
   def create
+    # function checked and needed
+    # everyone can create a project
     @project = Project.new(project_params)
+    authorize @project
     @project.user = current_user
+
     if @project.save
       redirect_to projects_path
     else
@@ -31,17 +43,25 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+    # function checked and needed
+    # only person who create can edit 
     @project = Project.find(params[:id])
+    @user = current_user
+    authorize @project
   end
 
   def update
+    # function checked and needed
+    # only person who create can update
     @project = Project.find(params[:id])
+    authorize @project
     @project.update(project_params)
     redirect_to projects_path
   end
 
   def destroy
     @project = Project.find(params[:id])
+    authorize @project
     @project.destroy
     redirect_to projects_path
   end
---
+++ app/policies/project_policy.rb
@@ -0,0 +1,21 @@
+class ProjectPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+
+  def create? 
+    return true
+  end 
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end     
+
+end
