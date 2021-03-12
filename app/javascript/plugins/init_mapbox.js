b878f1a3018489e4eb5832a86775ac414593b2d9
Megan Grosz <megrosz@gmail.com>
2021-03-12 10:20:02 +0100

    small change to map zoom and spaced voting avatars

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -28,7 +28,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 100, maxZoom: 20, duration: 450 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 450 });
 };
 
 const initMapbox = () => {
